b229027ab2b2cf50179b86e2b41c69a5c7db2136
cherry-bit <69865175+cherry-bit@users.noreply.github.com>
2020-12-18 21:53:44 -0500

    Add files via upload

---
+++ dataType.js
@@ -0,0 +1,46 @@
+var name= "cherry";
+console.log(name);
+//console.log(typeof(name));
+var name="cherry";
+  console.log(name);
+  console.log(typeof(name));
+  var num=100;
+  console.log(num);
+  console.log(typeof(num));
+  var bool=true;
+  console.log(bool);
+  console.log(typeof(bool));
+  var object;
+  console.log(object);
+  console.log(typeof(object));
+
+  var object={'name':'cherry',
+  'age':'14'  
+    }
+console.log(object);
+
+
+var arr=[10,20,30,40,50];
+  console.log(arr);
+  console.log(arr[0])
+  
+  var arr2=["monday",44,true,88.5]
+   console.log(arr2);
+  console.log(arr2[0])
+
+  var arr=[[10,20],[30,40]]
+  console.log(arr);
+  console.log(arr[1][1])
+
+  function setup() {
+    var a=[11,22,33];
+     a.push(10);
+     a.push(20);
+     
+     
+     console.log(a);
+     a.pop();
+      console.log(a);
+     a.pop();
+      console.log(a);
+     
